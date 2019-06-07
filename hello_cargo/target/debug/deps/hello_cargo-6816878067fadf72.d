5acd5b19c6c3f9836d879f907a2759f141b31b0a
Robert Thompson <rbtprograms@gmail.com>
2019-06-06 21:16:45 -0700

    compiling code in rust is p cool

---
+++ hello_cargo/target/debug/deps/hello_cargo-6816878067fadf72.d
@@ -0,0 +1,5 @@
+/Users/robertthompson/code/lets_learn_rust/hello_cargo/target/debug/deps/hello_cargo-6816878067fadf72.rmeta: src/main.rs
+
+/Users/robertthompson/code/lets_learn_rust/hello_cargo/target/debug/deps/hello_cargo-6816878067fadf72.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/robertthompson/code/lets_learn_rust/hello_cargo/target/debug/hello_cargo.dSYM: /Users/robertthompson/code/lets_learn_rust/hello_cargo/src/main.rs
